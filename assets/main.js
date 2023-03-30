b3cfe0ca1dd77a3667af3834f89d99d4a99273b6
abhijeetmohol <abhijeetmohol3101@gmail.com>
2023-03-30 12:46:44 +0530

    first commit

---
+++ assets/main.js
@@ -0,0 +1,24 @@
+// trying to use let multiple times (numerical)
+let a="10";
+b="20";
+console.log("a=10")
+console.log("b=20")
+console.log("The multiplication of above variable is", a*b);
+
+// trying to use let multiple times (alphabets)
+let first_name="Abhijeet";
+last_name="Mohol";
+console.log("first_name=Abhijeet")
+console.log("last_name=Mohol")
+console.log("The full name is", first_name+last_name);
+
+// trying to use let multiple times (datatypes)
+let w="string";
+x="numerical";
+y="symballs";
+z="etc.";
+console.log("Followings are the data-types :")
+console.log(w)
+console.log(x)
+console.log(y)
+console.log(z)
